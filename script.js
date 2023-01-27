8fb9f0734eede457344e486a3251ac2508eb82f8
LENILSON BATISTA <87871421+lenysss@users.noreply.github.com>
2023-01-27 20:33:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,224 @@
+function showOverlay() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function hideOverlay() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+  function showOverlaysam() {
+    document.getElementById("overlaysam").style.display = "block";
+  }
+  
+  function hideOverlaysam() {
+    document.getElementById("overlaysam").style.display = "none";
+  }
+  function showOverlaycass() {
+    document.getElementById("overlaycass").style.display = "block";
+  }
+  
+  function hideOverlaycass() {
+    document.getElementById("overlaycass").style.display = "none";
+  }
+  function showOverlaycrow() {
+    document.getElementById("overlaycrow").style.display = "block";
+  }
+  
+  function hideOverlaycrow() {
+    document.getElementById("overlaycrow").style.display = "none";
+  }
+  function showOverlaybobby() {
+    document.getElementById("overlaybobby").style.display = "block";
+  }
+  
+  function hideOverlaybobby() {
+    document.getElementById("overlaybobby").style.display = "none";
+  }
+  function showTab() {
+      var x = document.getElementById("secretTab");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab1() {
+      var x = document.getElementById("secret1");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab2() {
+      var x = document.getElementById("secret2");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab3() {
+      var x = document.getElementById("secret3");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab4() {
+      var x = document.getElementById("secret4");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab5() {
+      var x = document.getElementById("secret5");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab6() {
+      var x = document.getElementById("secret6");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab7() {
+      var x = document.getElementById("secret7");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab8() {
+      var x = document.getElementById("secret8");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab9() {
+      var x = document.getElementById("secret9");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab10() {
+      var x = document.getElementById("secret10");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab11() {
+      var x = document.getElementById("secret11");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab12() {
+      var x = document.getElementById("secret12");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab13() {
+      var x = document.getElementById("secret13");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab14() {
+      var x = document.getElementById("secret14");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function showTab15() {
+      var x = document.getElementById("secret15");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+function t1(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-1x1/", "_blank");
+}
+function t2(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-2x1/", "_blank");
+}
+function t3(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-3x1/", "_blank");
+}
+function t4(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-4x1/", "_blank");
+}
+function t5(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-5x1/", "_blank");
+}
+function t6(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-6x1/", "_blank");
+}
+function t7(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-7x1/", "_blank");
+}
+function t8(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-8x1/", "_blank");
+}
+function t9(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-9x1/", "_blank");
+}
+function t10(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-10x1/", "_blank");
+}
+function t11(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-11x1/", "_blank");
+}
+function t12(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-12x1/", "_blank");
+}
+function t13(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-13x1/", "_blank");
+}
+function t14(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-14x1/", "_blank");
+}
+function t15(){
+  var p1 = window.document.getElementById('t1')
+  window.open("https://megafilmeshdd.net/episodios/sobrenatural-15x1/", "_blank");
+}
